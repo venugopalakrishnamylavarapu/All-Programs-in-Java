7f07dfc2745c77dfb2686d0477ae173e1b6332dc
Krishna.M <venugopalakrishnamylavarapu@gmail.com>
2022-08-19 19:15:07 +0530

    ArrayPrac have been modified and the class for binarysearch in two d arrays has been added

--- Arrays programs/src/com/Arrays/ArraysPrac.java
+++ Arrays programs/src/com/Arrays/ArraysPrac.java
@@ -1,5 +1,6 @@
 package com.Arrays;
 
+import javax.crypto.spec.PSource;
 import java.util.*;
 
 import static java.lang.System.in;
@@ -7,48 +8,24 @@ import static java.lang.System.in;
 
 public class ArraysPrac {
     public static void main(String[] args) {
+        //code
         Scanner sc = new Scanner(in);
         int m = sc.nextInt();
         int[] arr = new int[m];
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = sc.nextInt();
+        for (int i = 0; i < m; i++) {
+           arr[i]=sc.nextInt();
         }
-        Arrays.sort(arr);
-        System.out.println("The entered array is:" + Arrays.toString(arr));
-        int length = duplicateRemoved(arr, arr.length);
-        for (int i = 0; i < length; i++) {
-            System.out.print(arr[i] + " ");
-        }
-    }
+        System.out.println("The given matric is");
+        System.out.println("Enter the target:");
+        int value = sc.nextInt();
+        System.out.println("The value of the "+value+" smallest element is: "+max(arr,value));
 
-    static int duplicateRemoved(int[] arr, int length) {
-        int[] temp = new int[length];
-        int j = 0;
-        for (int i = 0; i < length - 1; i++) {
-            if (arr[i] != arr[i + 1]) {
-                temp[j] = arr[i];
-                j++;
-            }
-        }
-        temp[j++] = arr[length - 1];
-        for (int i = 0; i <= j; i++) {
-            arr[i] = temp[i];
-        }
-        return j;
-//        int[] temp = new int[length];
-//        int j = 0;
-//        for (int i = 0; i < length - 1; i++) {
-//            if (arr[i] != arr[i + 1]) {
-//                temp[j] = arr[i];
-//                j++;
-//            }
-//        }
-//        temp[j++] = arr[length - 1];
-////        if (j >= 0) System.arraycopy(temp, 0, arr, 0, j);
-//        for (int i = 0; i <= j; i++) {          //the above system.arraycopy is short cut of this long for loop form to copy array.
-//            arr[i] = temp[i];                    //temp lo unna elements ni main array(function lo unadi) lo  update cheystunnam
-//        }
-//        return j;
-//    }
+    }
+    static int max(int [] arr,int value)
+    {
+        int ans=-1;
+        Arrays.sort(arr);
+       ans=arr[value];
+        return ans;
     }
 }
---
+++ Arrays programs/src/com/Arrays/BinarySearchInTwoDArrays.java
@@ -0,0 +1,52 @@
+package com.Arrays;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+import static java.lang.System.in;
+
+public class BinarySearchInTwoDArrays {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(in);
+//        System.out.println("enter the number of rows:");
+//        int m=sc.nextInt();
+//        System.out.println("enter the number of cols:");
+//        int n=sc.nextInt();
+        int[][] arr={
+                {1,2,3},
+                {4,5,6},
+                {7,8,9}};
+//        System.out.println("Enter the values in the matrix:");
+//        for (int i = 0; i < arr.length; i++) {
+//            for (int j = 0; j < arr[i].length; j++) {
+//                arr[i][j]=sc.nextInt();
+//            }
+//        }
+//        for (int row = 0; row < arr.length; row++) {      //we used the short cut here since the array in the array is considered as one index
+//            System.out.println("The given matrix array is:"+ Arrays.toString(arr[row])); //and here we print the array with index(which consists of arrays)
+//        }
+//        System.out.println("Enter the target:");
+        int target=sc.nextInt();
+        System.out.println("The index of the target value in the matrix is:");
+        int[] index=binarySearchIn2dArrays(arr,target);
+        System.out.println(Arrays.toString(index));
+    }
+    static int[] binarySearchIn2dArrays(int[][] matrix,int target)
+    {
+        int row=0;
+        int col=matrix.length-1;
+        while (row<=matrix.length&&col>=0)
+            if(matrix[row][col]==target)
+            {
+                return new int[]{row,col};
+            }
+             else if(target<matrix[row][col]) {
+                col--;
+            }
+             else if(target>matrix[row][col])         //condition entantey target>matrix[row][col]
+            {
+                row++;
+            }
+        return new int[]{-1,-1};
+    }
+}
