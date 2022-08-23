bc289620bd465e6dcccb86677493fb3eae9852ee
Krishna.M <venugopalakrishnamylavarapu@gmail.com>
2022-08-23 06:16:25 +0530

    Code has been modified

--- OOPS_CONCEPTS/src/AudioPlayer.java
+++ OOPS_CONCEPTS/src/AudioPlayer.java
@@ -10,26 +10,20 @@ public class AudioPlayer {
         File file=new File("[iSongs.info] 01 - Follow Follow-1.wav");           //audio files only .wav format lo undali and File class ni vadam ikkada
          AudioInputStream audioInputStream= AudioSystem.getAudioInputStream(file);      //inputstreeam audio ni tiskuntaniki,audiosystem.get ani undi kada adi file ni tiskuntaniki audio system loki
         Clip clip=AudioSystem.getClip();        //clip aney method lo clip ni open cheya tam close cheyatam and so on methods untai so danni manam call cheysam
-
+        clip.open(audioInputStream);
         String str ="";
         while(!str.equals("Q"))
         {
             System.out.println("Enter the option:");
-            System.out.print("option Q:-to exit ||option S:-to Start ||option R:-to Reset ||option H:-to stop:-");
-            str=sc.next();
-            switch (str)
-            {
-                case ("Q"):clip.close();
-                break;
-                case  ("S"):clip.open();
-                break;
-                case ("R"):clip.setMicrosecondPosition(0);
-                break;
-                case ("H"):clip.stop();
-                break;
-                default:
-                    System.out.println("Not a valid Option");
-                    break;
+            System.out.println("option S:-to Start ||option R:-to Reset ||option H:-to stop ||option Q:-to exit:-");
+            str=sc.nextLine();
+            str=str.toUpperCase();
+            switch (str) {
+                case ("Q") -> clip.close();
+                case ("S") -> clip.start();
+                case ("R") -> clip.setMicrosecondPosition(0);
+                case ("H") -> clip.stop();
+                default -> System.out.println("Not a valid Option");
             }
             System.out.println("malli next pata lo kaludham");
         }
