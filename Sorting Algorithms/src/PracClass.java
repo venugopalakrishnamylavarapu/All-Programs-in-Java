60f40371ac8e3fb4168d74413ec6db0cd60c3dd5
Krishna <venugopalakrishnamylavarapu@gmail.com>
2022-10-11 18:03:03 +0530

    new files have been added

--- Sorting Algorithms/src/PracClass.java
+++ Sorting Algorithms/src/PracClass.java
@@ -8,34 +8,27 @@ import static java.lang.System.in;
 public class PracClass {
     public static void main(String[] args) {
         Scanner sc = new Scanner(in);
-        int [] arr={8,2,5,1,72,5,8,7,3,6,9,10,4,5,2,1,9,3,4,6};
-        insertionSort(arr);
-        System.out.println(Arrays.toString(arr));
+        int[] arr={4,8,9,10,14,16,19,47,59,86};
+        PracClass obj=new PracClass();
+        System.out.println(obj.binarySearch(arr,8));
+
     }
 
-     static void insertionSort(int[] arr) {
-         for (int i = 0; i <arr.length ; i++) {
-             for (int j = 1; j < arr.length-i; j++) {
-                 if(arr[j]<arr[j-1])
-                 {
-                     swap(arr,j,j-1);
-                 }
-             }
-         }
-     }
-//     static int maxIndex(int[] arr,int start,int end)
-//     {      int max=0;
-//         for (int i = start; i <=end; i++) {
-//             if(arr[i]>arr[max])
-//             {
-//                max=i;
-//             }
-//         }
-//         return max;
-//     }
-  static void swap(int[] arr, int i, int j) {
-        int temp=arr[i];
-        arr[i]=arr[j];
-        arr[j]=temp;
+    public int binarySearch(int[] arr, int target) {
+        int low = 0;
+        int high = arr.length ;
+        while (low < high) {
+            int mid = (low + high) / 2;
+            if (arr[mid] == target) {
+                return mid;
+            }
+            if (arr[mid] > target) {
+                high = mid - 1;
+            }
+            if (arr[mid] < target) {
+                low = mid + 1;
+            }
+        }
+        return -1;
     }
 }
