72c27daa612d5711ab134db8085313b7bf75b3fb
Krishna <venugopalakrishnamylavarapu@gmail.com>
2022-12-20 17:19:47 +0530

    New files have been added

--- LeetCodeProblems/src/PracClass.java
+++ LeetCodeProblems/src/PracClass.java
@@ -1,35 +1,28 @@
-//this is a practice class so this doesnot contain any leetcode problem solutions
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
+//this is a practice class so this does not contain any leetcode problem solutions
+import java.util.*;
 
 import static java.lang.System.in;
 
 public class PracClass {
-    public static int meth1(String str)
-    {
-        if(str.length()==0)
-            return 0;
-        int count=1;
-        for(int i=0;i<str.length();i++)
-        {
-            if(str.charAt(i)==' ')
-                count++;
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<Character>();
+        for (char c : s.toCharArray()) {
+            if (c == '(')
+                stack.push(')');
+            else if (c == '{')
+                stack.push('}');
+            else if (c == '[')
+                stack.push(']');
+            else if (stack.isEmpty() || stack.pop() != c)
+                return false;
         }
-        return count;
+        return stack.isEmpty();
     }
 
     public static void main(String[] args) {
-    String str1="Hello, my name is venu";
-    String str2="Hello";
-    String str3="love live! mu'sic forever";
-    String str4="";
-        System.out.println(new PracClass().meth1(str1));
-        System.out.println(new PracClass().meth1(str2));
-        System.out.println(new PracClass().meth1(str3));
-        System.out.println(new PracClass().meth1(str4));
-
+        String str = "()[]{}";
+       // String str="{)";
+        System.out.println(new PracClass().isValid(str));
     }
 
 }
